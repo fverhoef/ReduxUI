9ac6300b6697a69bb528be0c8cd4cea648ca77b9
Frederik Verhoef <frederikverhoef@gmail.com>
2021-03-04 18:07:35 +0000

    Refactored common portrait texture update code into a separate method.

--- Modules/UnitFrames/Elements/Portrait.lua
+++ Modules/UnitFrames/Elements/Portrait.lua
@@ -6,8 +6,7 @@ local oUF = ns.oUF or oUF
 function UF:CreatePortrait()
     self.Portrait = self:CreateTexture("$parentPortrait", "BACKGROUND")
     self.Portrait.PostUpdate = function()
-        self.Portrait:SetDesaturated(not UnitIsConnected(self.unit))
-        self:UpdatePortrait()
+        self:UpdatePortraitTexture()
     end
 
     self.PortraitHolder = CreateFrame("Frame", "$parentPortraitHolder", self)
@@ -28,6 +27,12 @@ function UF:UpdatePortrait()
     if config.enabled then
         self:EnableElement("Portrait")
 
+        if config.class then
+            config.model = false
+        elseif config.model then
+            config.class = false
+        end
+
         self.Portrait:SetSize(unpack(config.size))
 
         local xOffset = self.config.border.enabled and self.config.border.size / 2 or 0
@@ -45,29 +50,7 @@ function UF:UpdatePortrait()
             self.Portrait:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT", -xOffset, yOffset)
         end
 
-        if config.class and UnitIsPlayer(self.unit) then
-            local coords = _G.CLASS_ICON_TCOORDS[select(2, UnitClass(self.unit))]
-            if coords then
-                self.Portrait:SetTexture("Interface\\TargetingFrame\\UI-Classes-Circles")
-                local l, r, t, b = unpack(coords)
-                if not config.round then
-                    local width = r - l
-                    local height = b - t
-
-                    l = l + 0.15 * width
-                    r = r - 0.15 * width
-                    t = t + 0.15 * height
-                    b = b - 0.15 * height
-                end
-                self.Portrait:SetTexCoord(l, r, t, b)
-            end
-        elseif config.round then
-            SetPortraitTexture(self.Portrait, self.unit)
-            self.Portrait:SetTexCoord(0, 1, 0, 1)
-        else
-            SetPortraitTexture(self.Portrait, self.unit)
-            self.Portrait:SetTexCoord(0.15, 0.85, 0.15, 0.85)
-        end
+        self:UpdatePortraitTexture()
 
         self.PortraitHolder.Border:SetShown(config.detached and config.border.enabled)
         self.PortraitHolder:SetBorderSize(config.border.size)
@@ -78,3 +61,42 @@ function UF:UpdatePortrait()
 end
 
 oUF:RegisterMetaFunction("UpdatePortrait", UF.UpdatePortrait)
+
+function UF:UpdatePortraitTexture()
+    if not self.Portrait then
+        return
+    end
+    
+    local config = self.config.portrait
+    if not config.enabled then
+        return
+    end
+    
+    self.Portrait:SetDesaturated(not UnitIsConnected(self.unit))
+
+    if config.class and UnitIsPlayer(self.unit) then
+        local coords = _G.CLASS_ICON_TCOORDS[select(2, UnitClass(self.unit))]
+        if coords then
+            self.Portrait:SetTexture("Interface\\TargetingFrame\\UI-Classes-Circles")
+            local l, r, t, b = unpack(coords)
+            if not config.round then
+                local width = r - l
+                local height = b - t
+
+                l = l + 0.15 * width
+                r = r - 0.15 * width
+                t = t + 0.15 * height
+                b = b - 0.15 * height
+            end
+            self.Portrait:SetTexCoord(l, r, t, b)
+        end
+    elseif config.round then
+        SetPortraitTexture(self.Portrait, self.unit)
+        self.Portrait:SetTexCoord(0, 1, 0, 1)
+    else
+        SetPortraitTexture(self.Portrait, self.unit)
+        self.Portrait:SetTexCoord(0.15, 0.85, 0.15, 0.85)
+    end
+end
+
+oUF:RegisterMetaFunction("UpdatePortraitTexture", UF.UpdatePortraitTexture)
