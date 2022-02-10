f0e46abb443c0908333d640da42d503dcd51156a
Frederik Verhoef <frederikverhoef@gmail.com>
2022-02-10 14:03:09 +0000

    Fixed bug in totems code when using TBC.

--- Libs/oUF/elements/totems.lua
+++ Libs/oUF/elements/totems.lua
@@ -159,7 +159,7 @@ local function Enable(self)
 		TotemFrame:UnregisterEvent('PLAYER_TOTEM_UPDATE')
 		TotemFrame:UnregisterEvent('PLAYER_ENTERING_WORLD')
 		TotemFrame:UnregisterEvent('UPDATE_SHAPESHIFT_FORM')
-		TotemFrame:UnregisterEvent('PLAYER_TALENT_UPDATE')
+		if oUF.isRetail then TotemFrame:UnregisterEvent('PLAYER_TALENT_UPDATE') end
 
 		return true
 	end
@@ -175,7 +175,7 @@ local function Disable(self)
 		TotemFrame:RegisterEvent('PLAYER_TOTEM_UPDATE')
 		TotemFrame:RegisterEvent('PLAYER_ENTERING_WORLD')
 		TotemFrame:RegisterEvent('UPDATE_SHAPESHIFT_FORM')
-		TotemFrame:RegisterEvent('PLAYER_TALENT_UPDATE')
+		if oUF.isRetail then TotemFrame:RegisterEvent('PLAYER_TALENT_UPDATE') end
 
 		self:UnregisterEvent('PLAYER_TOTEM_UPDATE', Path)
 	end
