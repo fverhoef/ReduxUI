1feec639113bc9cfcee737c78beeb30b31644b7c
Frederik Verhoef <frederikverhoef@gmail.com>
2022-11-16 21:08:27 +0000

    Adding 'disabled' parameter

--- Config/OptionsHelper.lua
+++ Config/OptionsHelper.lua
@@ -10,13 +10,14 @@ R.FONT_OUTLINES = {"NONE", "OUTLINE", "THICKOUTLINE"}
 R.FONT_MIN_SIZE = 4
 R.FONT_MAX_SIZE = 30
 
-function R:CreateColorOption(name, desc, order, hidden, hasAlpha, default, get, postSet)
+function R:CreateColorOption(name, desc, order, hidden, hasAlpha, default, get, postSet, disabled)
     return {
         type = "color",
         name = name,
         desc = desc,
         order = order,
         hidden = hidden,
+        disabled = disabled,
         hasAlpha = hasAlpha,
         get = function()
             local color = get()
@@ -37,13 +38,14 @@ function R:CreateColorOption(name, desc, order, hidden, hasAlpha, default, get, 
     }
 end
 
-function R:CreateFontOption(name, desc, order, hidden, get, set, postSet, confirm)
+function R:CreateFontOption(name, desc, order, hidden, get, set, postSet, confirm, disabled)
     return {
         type = "select",
         name = name,
         desc = desc,
         order = order,
         hidden = hidden,
+        disabled = disabled,
         dialogControl = "LSM30_Font",
         values = R.Libs.SharedMedia:HashTable("font"),
         get = function() for key, font in pairs(R.Libs.SharedMedia:HashTable("font")) do if get() == font then return key end end end,
@@ -55,13 +57,14 @@ function R:CreateFontOption(name, desc, order, hidden, get, set, postSet, confir
     }
 end
 
-function R:CreateInputOption(name, desc, order, hidden, get, set, postSet)
+function R:CreateInputOption(name, desc, order, hidden, get, set, postSet, disabled)
     return {
         type = "input",
         name = name,
         desc = desc,
         order = order,
         hidden = hidden,
+        disabled = disabled,
         get = get,
         set = function(_, val)
             set(val)
@@ -70,13 +73,14 @@ function R:CreateInputOption(name, desc, order, hidden, get, set, postSet)
     }
 end
 
-function R:CreateRangeOption(name, desc, order, hidden, min, max, softMax, step, get, set, postSet)
+function R:CreateRangeOption(name, desc, order, hidden, min, max, softMax, step, get, set, postSet, disabled)
     return {
         type = "range",
         name = name,
         desc = desc,
         order = order,
         hidden = hidden,
+        disabled = disabled,
         min = min,
         max = max,
         softMax = softMax,
@@ -89,15 +93,16 @@ function R:CreateRangeOption(name, desc, order, hidden, min, max, softMax, step,
     }
 end
 
-function R:CreateSelectOption(name, desc, order, hidden, values, get, set, postSet)
+function R:CreateSelectOption(name, desc, order, hidden, values, get, set, postSet, disabled)
     return {
         type = "select",
         name = name,
         desc = desc,
         order = order,
         hidden = hidden,
+        disabled = disabled,
         values = values,
-        get = function() for key, value in ipairs(values) do if value == get() then return key end end end,
+        get = function() for key, value in pairs(values) do if value == get() then return key end end end,
         set = function(_, key)
             set(values[key])
             if postSet then postSet() end
@@ -105,13 +110,14 @@ function R:CreateSelectOption(name, desc, order, hidden, values, get, set, postS
     }
 end
 
-function R:CreateStatusBarOption(name, desc, order, hidden, get, set, postSet)
+function R:CreateStatusBarOption(name, desc, order, hidden, get, set, postSet, disabled)
     return {
         type = "select",
         name = name,
         desc = desc,
         order = order,
         hidden = hidden,
+        disabled = disabled,
         dialogControl = "LSM30_Statusbar",
         values = R.Libs.SharedMedia:HashTable("statusbar"),
         get = function() for key, texture in pairs(R.Libs.SharedMedia:HashTable("statusbar")) do if get() == texture then return key end end end,
@@ -122,7 +128,7 @@ function R:CreateStatusBarOption(name, desc, order, hidden, get, set, postSet)
     }
 end
 
-function R:CreateToggleOption(name, desc, order, width, hidden, get, set, postSet, confirm)
+function R:CreateToggleOption(name, desc, order, width, hidden, get, set, postSet, confirm, disabled)
     return {
         type = "toggle",
         name = name,
@@ -130,6 +136,7 @@ function R:CreateToggleOption(name, desc, order, width, hidden, get, set, postSe
         order = order,
         width = width,
         hidden = hidden,
+        disabled = disabled,
         get = function() return get() end,
         set = function(_, value)
             set(value)
