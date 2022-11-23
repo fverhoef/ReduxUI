0f46c739004325f29c96bfcd620ae83091690bce
Frederik Verhoef <frederikverhoef@gmail.com>
2022-11-23 10:38:24 +0000

    Don't touch raid frame manager

--- Modules/UnitFrames/Units/Raid.lua
+++ Modules/UnitFrames/Units/Raid.lua
@@ -5,10 +5,15 @@ local oUF = ns.oUF or oUF
 
 function UF:SpawnRaidHeader()
     if UF.config.raid.enabled then
-        CompactRaidFrameManager_SetSetting("IsShown", "0")
         _G.UIParent:UnregisterEvent("GROUP_ROSTER_UPDATE")
-        _G.CompactRaidFrameManager:UnregisterAllEvents()
-        _G.CompactRaidFrameManager:SetParent(R.HiddenFrame)
+
+		if _G.CompactRaidFrameContainer then
+			_G.CompactRaidFrameContainer:UnregisterAllEvents()
+		end
+
+        --CompactRaidFrameManager_SetSetting("IsShown", "0")
+        --_G.CompactRaidFrameManager:UnregisterAllEvents()
+        --_G.CompactRaidFrameManager:SetParent(R.HiddenFrame)
     end
 
     return UF:SpawnHeader("Raid", NUM_RAID_GROUPS, RaidMixin, UF.config.raid, UF.defaults.raid)
