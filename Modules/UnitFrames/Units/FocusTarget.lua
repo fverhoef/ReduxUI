8152f3ca953009987fb0331a7f407a54cb931ab4
Frederik Verhoef <frederikverhoef@gmail.com>
2022-11-25 00:15:54 +0000

    Added dragonflight style support for focus, focus target, and target's target

--- Modules/UnitFrames/Units/FocusTarget.lua
+++ Modules/UnitFrames/Units/FocusTarget.lua
@@ -68,6 +68,67 @@ function FocusTargetMixin:PostConfigure()
             self.RaidTargetIndicator:ClearAllPoints()
             self.RaidTargetIndicator:SetPoint("CENTER", self.Portrait, "TOP")
         end
+    elseif self.config.style == UF.Styles.Dragonflight then
+        self:SetSize(115, 44)
+        self:ClearAllPoints()
+        self:SetPoint("LEFT", UF.frames.focus, "RIGHT", 5, 0)
+
+        self.Border:Hide()
+        self.Inlay:Hide()
+        self.Shadow:Hide()
+
+        if not self.Artwork then
+            self.Artwork = self:CreateTexture("$parentArtwork", "BORDER", nil, 7)
+        end
+        self.Artwork:SetTexture(R.media.textures.unitFrames.dragonflight.unitFrame)
+        self.Artwork:SetTexCoord(0.75, 0.9794921875, 0, 0.173828125)
+        self.Artwork:ClearAllPoints()
+        self.Artwork:SetAllPoints()
+        self.Artwork:Show()
+
+        self.Name:Show()
+        self.Name:SetFont(self.config.name.font, 11, "OUTLINE")
+        self.Name:SetJustifyH("LEFT")
+        self.Name:SetShadowOffset(0, 0)
+
+        self.Name:ClearAllPoints()
+        self.Name:SetPoint("TOPLEFT", 42, -3)
+        self.Name:SetPoint("BOTTOMRIGHT", self, "TOPRIGHT", -6, -12)
+        self:Tag(self.Name, "[name:sub(15)]")
+
+        self.Level:Hide()
+
+        self.PortraitHolder.Separator:Hide()
+
+        self:DisableElement("Portrait")
+        self.Portrait = self.PortraitHolder.PortraitRound
+        self.PortraitHolder:SetSize(33, 33)
+        self.PortraitHolder:ClearAllPoints()
+        self.PortraitHolder:SetPoint("TOPLEFT", 7, -6)
+        self:EnableElement("Portrait")
+
+        self.Health:ClearAllPoints()
+        self.Health:SetPoint("TOPLEFT", 42, -14)
+        self.Health:SetPoint("BOTTOMRIGHT", -7, 20)
+
+        self.Health.Value:Hide()
+        self.Health.Percent:Hide()
+
+        self:EnableElement("Power")
+        self.Power:ClearAllPoints()
+        self.Power:SetPoint("TOPLEFT", 40, -24)
+        self.Power:SetPoint("BOTTOMRIGHT", -7, 12)
+
+        self.Power.Border:Hide()
+        self.Power.Separator:Hide()
+        self.Power.Value:Hide()
+        self.Power.Percent:Hide()
+
+        if self.RaidTargetIndicator then
+            self.RaidTargetIndicator:SetSize(18, 18)
+            self.RaidTargetIndicator:ClearAllPoints()
+            self.RaidTargetIndicator:SetPoint("CENTER", self.Portrait, "TOP")
+        end
     else
         if self.Artwork then
             self.Artwork:Hide()
--- Modules/UnitFrames/Units/Party.lua
+++ Modules/UnitFrames/Units/Party.lua
@@ -20,19 +20,21 @@ function PartyMixin:PostConfigure()
 
         if not self.Artwork then
             self.Artwork = self:CreateTexture("$parentArtwork", "BORDER", nil, 7)
-            self.Artwork:SetTexCoord(0, 240 / 256, 0, 98 / 128)
-            self.Artwork:SetAllPoints()
         end
         self.Artwork:SetTexture(self.config.largeHealth and R.media.textures.unitFrames.vanilla.partyFrame_LargeHealth or R.media.textures.unitFrames.vanilla.partyFrame)
+        self.Artwork:SetTexCoord(0, 240 / 256, 0, 98 / 128)
+        self.Artwork:ClearAllPoints()
+        self.Artwork:SetAllPoints()
         self.Artwork:Show()
 
         if not self.Flash then
             self.Flash = self:CreateTexture("$parentFlash", "BACKGROUND", nil, 1)
-            self.Flash:SetTexCoord(0, 1, 0, 1)
-            self.Flash:SetPoint("CENTER", self, "CENTER", 0, -4)
-            self.Flash:SetSize(scale * 128, scale * 64)
         end
         self.Flash:SetTexture(self.config.largeHealth and R.media.textures.unitFrames.vanilla.partyFrame_LargeHealth_Flash or R.media.textures.unitFrames.vanilla.partyFrame_Flash)
+        self.Flash:SetTexCoord(0, 1, 0, 1)
+        self.Flash:ClearAllPoints()
+        self.Flash:SetPoint("CENTER", self, "CENTER", 0, -4)
+        self.Flash:SetSize(scale * 128, scale * 64)
         self.Flash:Hide()
 
         self.Name:Show()
@@ -69,7 +71,7 @@ function PartyMixin:PostConfigure()
             self.Health.Value:SetJustifyH("RIGHT")
             self.Health.Value:ClearAllPoints()
             self.Health.Value:SetPoint("RIGHT", self.Health, "RIGHT", -2, 0)
-    
+
             self.Health.Percent:SetFont(self.config.health.percent.font, 11, "OUTLINE")
             self.Health.Percent:SetJustifyH("LEFT")
             self.Health.Percent:ClearAllPoints()
@@ -88,6 +90,81 @@ function PartyMixin:PostConfigure()
             self.Power:SetPoint("TOPLEFT", self, "TOPLEFT", scale * 46, scale * -19)
             self.Power:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT", scale * -4, scale * 23)
         end
+
+        self.Power.Border:Hide()
+        self.Power.Separator:Hide()
+        self.Power.Value:Hide()
+        self.Power.Percent:Hide()
+
+        if self.RaidTargetIndicator then
+            self.RaidTargetIndicator:SetSize(18, 18)
+            self.RaidTargetIndicator:ClearAllPoints()
+            self.RaidTargetIndicator:SetPoint("CENTER", self.Portrait, "TOP")
+        end
+    elseif self.config.style == UF.Styles.Dragonflight then
+        self:SetSize(138, 53)
+
+        self.Border:Hide()
+        self.Inlay:Hide()
+        self.Shadow:Hide()
+
+        if not self.Artwork then
+            self.Artwork = self:CreateTexture("$parentArtwork", "BORDER", nil, 7)
+        end
+        self.Artwork:SetTexture(R.media.textures.unitFrames.dragonflight.unitFrame)
+        self.Artwork:SetTexCoord(0.75, 0.9794921875, 0, 0.173828125)
+        self.Artwork:ClearAllPoints()
+        self.Artwork:SetAllPoints()
+        self.Artwork:Show()
+
+        if not self.Flash then
+            self.Flash = self:CreateTexture("$parentFlash", "BACKGROUND", nil, 1)
+        end
+        self.Flash:SetTexture(R.media.textures.unitFrames.dragonflight.unitFrame)
+        self.Flash:SetTexCoord(0.75, 0.9794921875, 0.173828125, 0.34765625)
+        self.Flash:ClearAllPoints()
+        self.Flash:SetAllPoints()
+        self.Flash:Hide()
+
+        self.Name:Show()
+        self.Name:SetFont(self.config.name.font, 11, "OUTLINE")
+        self.Name:SetJustifyH("LEFT")
+        self.Name:SetShadowOffset(0, 0)
+
+        self.Name:ClearAllPoints()
+        self.Name:SetPoint("TOPLEFT", 50, -3)
+        self.Name:SetPoint("BOTTOMRIGHT", self, "TOPRIGHT", -6, -12)
+        self:Tag(self.Name, "[name:sub(15)]")
+
+        self.Level:Hide()
+
+        self.PortraitHolder.Separator:Hide()
+
+        self:DisableElement("Portrait")
+        self.Portrait = self.PortraitHolder.PortraitRound
+        self.PortraitHolder:SetSize(40, 40)
+        self.PortraitHolder:ClearAllPoints()
+        self.PortraitHolder:SetPoint("TOPLEFT", 7, -6)
+        self:EnableElement("Portrait")
+
+        self.Health:ClearAllPoints()
+        self.Health:SetPoint("TOPLEFT", 50, -16)
+        self.Health:SetPoint("BOTTOMRIGHT", -9, 24)
+
+        self.Health.Value:SetFont(self.config.health.value.font, 11, "OUTLINE")
+        self.Health.Value:SetJustifyH("RIGHT")
+        self.Health.Value:ClearAllPoints()
+        self.Health.Value:SetPoint("RIGHT", self.Health, "RIGHT", -2, 0)
+
+        self.Health.Percent:SetFont(self.config.health.percent.font, 11, "OUTLINE")
+        self.Health.Percent:SetJustifyH("LEFT")
+        self.Health.Percent:ClearAllPoints()
+        self.Health.Percent:SetPoint("LEFT", self.Health, "LEFT", 2, 0)
+
+        self:EnableElement("Power")
+        self.Power:ClearAllPoints()
+        self.Power:SetPoint("TOPLEFT", 48, -30)
+        self.Power:SetPoint("BOTTOMRIGHT", -9, 14)
 
         self.Power.Border:Hide()
         self.Power.Separator:Hide()
