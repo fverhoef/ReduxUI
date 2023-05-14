60e8706ff3622256d03580c9c3144e35e8b014fd
Frederik Verhoef <frederikverhoef@gmail.com>
2023-05-14 10:31:48 +0100

    Ignore batch files when zipping up builds.

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 mkdir C:\build\ReduxUI_%1\ReduxUI
 xcopy ..\ReduxUI C:\build\ReduxUI_%1\ReduxUI /s
-7z a C:\build\ReduxUI_%1\ReduxUI_%1.zip C:\build\ReduxUI_%1\ReduxUI
+7z a -xr!*.bat C:\build\ReduxUI_%1\ReduxUI_%1.zip C:\build\ReduxUI_%1\ReduxUI
